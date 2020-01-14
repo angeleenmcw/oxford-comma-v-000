9beeb25c514b1d215f2d63122f7c93287ef82fae
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-14 08:07:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   elseif array.length == 2 
-    return array[0] + " and " + 
+    return array[0] + " and " + array[1]
+  else array.length > 2 
+    return
+    
